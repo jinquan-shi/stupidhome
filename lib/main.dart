ce8633b16296e9b89c3499219f36aa2f2d703c37
2377671184 <sjq2377671184@gmail.com>
2020-07-25 01:12:58 +0800

    mqtt构建完成。

--- lib/main.dart
+++ lib/main.dart
@@ -39,11 +39,30 @@ class _MyHomePageState extends State<MyHomePage> {
   var _client;
   var _json;
   var _data;
+
+  //初始化连接及通知服务
   @override
   void initState() {
     super.initState();
     flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
+    var android = new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iOS = new IOSInitializationSettings();
+    var initSetttings = new InitializationSettings(android, iOS);
+    flutterLocalNotificationsPlugin.initialize(initSetttings,
+        onSelectNotification: onSelectNotification);
+    _client = MqttApp('broker.emqx.io', 'flutter_client', 1883);
     readJSON();
+  }
+  //通知初始化
+  Future onSelectNotification(String payload) {
+    debugPrint("payload : $payload");
+    showDialog(
+      context: context,
+      builder: (_) => new AlertDialog(
+        title: new Text('Notification'),
+        content: new Text('$payload'),
+      ),
+    );
   }
 
   //读取JSON
@@ -55,21 +74,25 @@ class _MyHomePageState extends State<MyHomePage> {
   //开锁控制
   void _unlock() {
     _client.sendMsg(_data["unlock"].toString(), "out");
-    if (_client.msgIn == 'OK') {
-      Fluttertoast.showToast(
-        msg: "开锁成功",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-      );
-      print("ok");
-    } else {
-      Fluttertoast.showToast(
-        msg: "开锁失败",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-      );
-      print("no");
-    }
+    Future.delayed(Duration(milliseconds: 5000), () {
+      if (_client.msgIn == 'OK') {
+        Fluttertoast.showToast(
+          msg: "开锁成功",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+        );
+        print("ok");
+        _showNotification();
+      } else {
+        Fluttertoast.showToast(
+          msg: "开锁失败",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+        );
+        print("no");
+        _showNotification();
+      }
+    });
   }
 
   //上锁控制
@@ -92,26 +115,24 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
 //发送通知
-  showNotification(String preload) async {
-    var android = new AndroidNotificationDetails(
-        'channel id', 'channel NAME', 'CHANNEL DESCRIPTION',
-        priority: Priority.High, importance: Importance.Max);
-    var iOS = new IOSNotificationDetails();
-    var platform = new NotificationDetails(android, iOS);
+  Future _showNotification() async {
+    //安卓的通知配置，必填参数是渠道id, 名称, 和描述, 可选填通知的图标，重要度等等。
+    var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+        '1', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High);
+    //IOS的通知配置
+    var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    //显示通知，其中 0 代表通知的 id，用于区分通知。
     await flutterLocalNotificationsPlugin.show(
-      0,
-      preload,
-      'Flutter Local Notification',
-      platform,
-    );
+        0, 'title', 'content', platformChannelSpecifics,
+        payload: 'complete');
   }
 
   @override
   Widget build(BuildContext context) {
-    Future(() {
-      _client = mqtt_app('broker.emqx.io', 'flutter_client', 1883);
-    }).then((value) => _client.subscribe("get"));
-
+    _client.connect().then((value) => _client.subscribe("get"));
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
--- lib/mqtt.dart
+++ lib/mqtt.dart
@@ -1,88 +1,81 @@
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
 
-class mqtt_app {
-  MqttServerClient client;
-  String msgIn;
-  String msgOut;
-
-  mqtt_app(String host, String identifier, int port) {
-    this.client = MqttServerClient.withPort(host, identifier, port);
-    connect();
+class MqttApp {
+  String host;
+  String identifier;
+  String msgIn, msgOut;
+  int port;
+  MqttServerClient _client;
+  MqttApp(String host, String identifier, int port) {
+    this.port = port;
+    this.identifier = identifier;
+    this.host = host;
+    this._client =
+        MqttServerClient.withPort(this.host, this.identifier, this.port);
   }
-  Future<void> connect() async {
-    client.logging(on: true);
-    client.onConnected = _onConnected;
-    client.onDisconnected = _onDisconnected;
-    client.onUnsubscribed = _onUnsubscribed;
 
-    client.onSubscribeFail = _onSubscribeFail;
-    client.pongCallback = _pong;
+  Future<void> connect() async {
+    _client.logging(on: true);
+    _client.onConnected = onConnected;
+    _client.onDisconnected = onDisconnected;
+    _client.onUnsubscribed = onUnsubscribed;
+    _client.onSubscribed = onSubscribed;
+    _client.onSubscribeFail = onSubscribeFail;
+    _client.pongCallback = pong;
+
     try {
-      await client.connect();
-      print("succeed");
+      await _client.connect();
     } catch (e) {
       print('Exception: $e');
-      client.disconnect();
+      _client.disconnect();
     }
-  }
-
-  void subscribe(String topic) {
-    client.subscribe(topic, MqttQos.atMostOnce);
-    client.onSubscribed(topic);
-    client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+    _client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
       final MqttPublishMessage message = c[0].payload;
       final payload =
           MqttPublishPayload.bytesToStringAsString(message.payload.message);
-      msgIn = payload;
-      print('Received message:$payload from topic: ${c[0].topic}>\n');
+      this.msgIn = payload;
+      print('Received message:$payload from topic: ${c[0].topic}>');
     });
+  }
+
+  void subscribe(String topic) {
+    _client.subscribe(topic, MqttQos.atLeastOnce);
   }
 
   void sendMsg(String msg, String pubTopic) {
     final builder = MqttClientPayloadBuilder();
     builder.addString(msg);
-    client.publishMessage(pubTopic, MqttQos.atLeastOnce, builder.payload);
-  }
-
-  void listen() {
-    client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
-      final MqttPublishMessage message = c[0].payload;
-
-      final payload =
-          MqttPublishPayload.bytesToStringAsString(message.payload.message);
-      print(payload);
-      print('Received message:$payload from topic: ${c[0].topic}>\n');
-    });
+    _client.publishMessage(pubTopic, MqttQos.atLeastOnce, builder.payload);
   }
 
   // 连接成功
-  void _onConnected() {
+  void onConnected() {
     print('Connected');
   }
 
-  // 连接断开
-  void _onDisconnected() {
+// 连接断开
+  void onDisconnected() {
     print('Disconnected');
   }
 
-  // 订阅主题成功
-  void _onSubscribed(String topic) {
+// 订阅主题成功
+  void onSubscribed(String topic) {
     print('Subscribed topic: $topic');
   }
 
-  // 订阅主题失败
-  void _onSubscribeFail(String topic) {
+// 订阅主题失败
+  void onSubscribeFail(String topic) {
     print('Failed to subscribe $topic');
   }
 
-  // 成功取消订阅
-  void _onUnsubscribed(String topic) {
+// 成功取消订阅
+  void onUnsubscribed(String topic) {
     print('Unsubscribed topic: $topic');
   }
 
-  // 收到 PING 响应
-  void _pong() {
+// 收到 PING 响应
+  void pong() {
     print('Ping response client callback invoked');
   }
 }
